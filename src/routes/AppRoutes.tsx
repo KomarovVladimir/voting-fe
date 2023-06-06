08cffe9887cfaa2b48f021e15f03ea7305b22960
KomarovVladimir <komarov2142@gmail.com>
2023-06-06 17:19:38 +0300

    Update the routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,19 @@
 import { Route, Routes } from "react-router";
+
 import { PublicRoutes } from "./PublicRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
 
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+import { Home } from "../pages/Home";
+
 export const AppRoutes = () => (
     <Routes>
-        <Route path="/" element={<PublicRoutes />}></Route>
+        <Route path="/" element={<PublicRoutes />}>
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+        </Route>
         <Route path="protected" element={<PrivateRoutes />}></Route>
     </Routes>
 );
