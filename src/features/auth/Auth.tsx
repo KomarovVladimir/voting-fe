f3eb4c6f36d1c2e1e5274c54e829c01ae076935f
KomarovVladimir <komarov2142@gmail.com>
2023-06-21 16:41:01 +0300

    Add a custom hook for the auth

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -10,23 +10,29 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export const LoginForm = () => (
-    <Paper>
-        <Box component="form">
-            <Stack direction="column" spacing={2} padding={3}>
-                <FormControl>
-                    <TextField label="Email" type="email" />
-                    <FormHelperText>Email helper</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <TextField label="Password" type="password" />
-                    <FormHelperText>Password helper</FormHelperText>
-                </FormControl>
-                <Button variant="contained">Log In</Button>
-                <MuiLink component={Link} to="../register">
-                    Don't have an account?
-                </MuiLink>
-            </Stack>
-        </Box>
-    </Paper>
-);
+import { useAuth } from "./useAuth";
+
+export const LoginForm = () => {
+    const { authRequest, result } = useAuth();
+
+    return (
+        <Paper>
+            <Box component="form">
+                <Stack direction="column" spacing={2} padding={3}>
+                    <FormControl>
+                        <TextField label="Email" type="email" />
+                        <FormHelperText>Email helper</FormHelperText>
+                    </FormControl>
+                    <FormControl>
+                        <TextField label="Password" type="password" />
+                        <FormHelperText>Password helper</FormHelperText>
+                    </FormControl>
+                    <Button variant="contained">Log In</Button>
+                    <MuiLink component={Link} to="../register">
+                        Don't have an account?
+                    </MuiLink>
+                </Stack>
+            </Box>
+        </Paper>
+    );
+};
--- src/features/auth/useAuth.tsx
+++ src/features/auth/useAuth.tsx
@@ -1,1 +1,14 @@
-export const useLoginForm = () => {};
+import { useEffect } from "react";
+import { useAuthRequestMutation } from "services/authApi";
+
+const testAuthData = { email: "admin@email.com", password: "admin" };
+
+export const useAuth = () => {
+    const [authRequest, result] = useAuthRequestMutation();
+
+    useEffect(() => {
+        authRequest(testAuthData);
+    }, []);
+
+    return { authRequest, result };
+};
