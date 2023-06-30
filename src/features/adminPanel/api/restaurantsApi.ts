449eda39f3bca7ba76b131e60c40943e6257ddd5
KomarovVladimir <komarov2142@gmail.com>
2023-06-30 05:23:00 +0300

    Move the type

--- src/features/adminPanel/api/restaurantsApi.ts
+++ src/features/adminPanel/api/restaurantsApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-import { Restaurant } from "types/Restaurant.type";
+import { Restaurant } from "../types/Restaurant.type";
 
 export const restaurantsApi = createApi({
     reducerPath: "restaurantsApi",
