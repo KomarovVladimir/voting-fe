c86a07a342ab5b29b1721a52aa9c910df28939e9
KomarovVladimir <komarov2142@gmail.com>
2023-08-09 20:20:14 +0300

    Add the rooms page layout

--- src/features/rooms/index.tsx
+++ src/features/rooms/index.tsx
@@ -1,10 +1,28 @@
-import { Stack } from "@mui/material";
+import { Button, Grid, Paper, Stack, Typography, styled } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+}));
 
 export const Rooms = () => {
     return (
-        <Stack direction="row" gap={40}>
-            <div>123</div>
-            <div>123</div>
-        </Stack>
+        <>
+            <Stack direction="row" gap={5} alignItems="center" mb={4}>
+                <Typography variant="h2">Rooms</Typography>
+                <Button variant="contained">+ New room</Button>
+            </Stack>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <Item>Restaurants</Item>
+                </Grid>
+                <Grid item xs={3}>
+                    <Item>Movies</Item>
+                </Grid>
+            </Grid>
+        </>
     );
 };
