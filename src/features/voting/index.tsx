b04ebf166ec35ee4c798b131c8e2bc50fedab01b
KomarovVladimir <komarov2142@gmail.com>
2023-08-18 03:34:55 +0300

    Add a basic breadcrumbs

--- src/features/voting/index.tsx
+++ src/features/voting/index.tsx
@@ -1,4 +1,5 @@
 import {
+    Box,
     Button,
     IconButton,
     List,
@@ -10,15 +11,17 @@ import {
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
+import { useState } from "react";
 
 import { RootState } from "app/store";
 import { ItemCreationDialog } from "features/rooms/RoomCreationDialog";
-import { useState } from "react";
+import { AppBar } from "components";
+
 import { addItem } from "./slice/votingSlice";
+import { Link } from "react-router-dom";
 
 export const Voting = () => {
     const { votingId } = useParams();
-    console.log(votingId);
 
     const items = useSelector(({ voting }: RootState) => voting);
     const [open, setOpen] = useState(false);
@@ -37,6 +40,31 @@ export const Voting = () => {
                 submitAction={addItem}
                 onClose={handleClose}
                 {...{ open }}
+            />
+            <AppBar
+                title={
+                    <>
+                        <Typography
+                            variant="h6"
+                            component={Link}
+                            to="/authorized/rooms"
+                            mr={2}
+                        >
+                            Rooms
+                        </Typography>
+                        <Typography variant="h6" component="span">
+                            Voting: {votingId}
+                        </Typography>
+                    </>
+                }
+                menu={
+                    <Box>
+                        <Button sx={{ color: "#fff" }} onClick={handleOpen}>
+                            + New Room
+                        </Button>
+                        <Button sx={{ color: "#fff" }}>Join</Button>
+                    </Box>
+                }
             />
             <Stack direction="row" gap={5} alignItems="center" mb={4}>
                 <Typography variant="h2">Voting Name</Typography>
