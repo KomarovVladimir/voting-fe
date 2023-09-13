3a481af6e34118349a592f28a384355fd15bccec
KomarovVladimir <komarov2142@gmail.com>
2023-09-13 20:49:44 +0300

    Move logic out of the Room component

--- src/features/room/Room/index.tsx
+++ src/features/room/Room/index.tsx
@@ -1,1 +1,64 @@
-export const Room = () => {};
+import {
+    Button,
+    Grid,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    TextField,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { Chat } from "features";
+
+import { useRoom } from "./useRoom";
+
+import { RoomAppBar } from "../RoomAppBar";
+
+export const Room = () => {
+    const { item, items, handleAddItem, handleRemoveItem, handleItemChange } =
+        useRoom();
+
+    return (
+        <>
+            <RoomAppBar />
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <List dense>
+                        {items?.map(({ id, name, votes }) => (
+                            <ListItem
+                                key={id}
+                                secondaryAction={
+                                    <IconButton
+                                        edge="end"
+                                        aria-label="delete"
+                                        onClick={handleRemoveItem(id)}
+                                    >
+                                        <CloseIcon />
+                                    </IconButton>
+                                }
+                            >
+                                <ListItemText
+                                    primary={name}
+                                    secondary={votes}
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                    <TextField
+                        label="Add item*"
+                        type="text"
+                        value={item}
+                        onChange={handleItemChange}
+                    />
+                    <Button variant="contained" onClick={handleAddItem}>
+                        Add Item
+                    </Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <Chat />
+                </Grid>
+            </Grid>
+        </>
+    );
+};
--- src/features/room/Room/useRoom.tsx
+++ src/features/room/Room/useRoom.tsx
@@ -1,1 +1,45 @@
-export const useRoom = () => {};
+import { ChangeEvent, useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+import {
+    useAddItemMutation,
+    useDeleteItemMutation,
+    useLazyGetItemsQuery,
+} from "../roomApi";
+
+export const useRoom = () => {
+    const { roomId } = useParams();
+    const [getItems, { data: itemsData }] = useLazyGetItemsQuery();
+    const [addItem] = useAddItemMutation();
+    const [deleteItem] = useDeleteItemMutation();
+    const [item, setItem] = useState("");
+
+    useEffect(() => {
+        if (roomId) {
+            getItems(roomId);
+        }
+    }, [roomId, getItems]);
+
+    const handleAddItem = () => {
+        addItem({
+            roomId,
+            name: "New Item",
+        });
+    };
+
+    const handleRemoveItem = (id: string) => () => {
+        deleteItem(id);
+    };
+
+    const handleItemChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setItem(e.target.value);
+    };
+
+    return {
+        item,
+        items: itemsData?.items,
+        handleAddItem,
+        handleRemoveItem,
+        handleItemChange,
+    };
+};
