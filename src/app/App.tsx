5b25a210784e436d76f7e0aac17e70e5957836bb
KomarovVladimir <komarov2142@gmail.com>
2023-09-17 17:02:59 +0300

    Add a dark theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
@@ -9,15 +9,23 @@ import { AppRoutes } from "routing";
 
 import { store } from "./store";
 
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
 export const App = () => {
     return (
         <StrictMode>
             <AuthProvider>
                 <Provider store={store}>
-                    <BrowserRouter>
+                    <ThemeProvider theme={darkTheme}>
                         <CssBaseline />
-                        <AppRoutes />
-                    </BrowserRouter>
+                        <BrowserRouter>
+                            <AppRoutes />
+                        </BrowserRouter>
+                    </ThemeProvider>
                 </Provider>
             </AuthProvider>
         </StrictMode>
