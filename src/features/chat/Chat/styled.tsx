cee6f3f64e84be80f7036eb33015694b8c791e97
KomarovVladimir <komarov2142@gmail.com>
2023-09-17 17:00:54 +0300

    Update styled.tsx

--- src/features/chat/Chat/styled.tsx
+++ src/features/chat/Chat/styled.tsx
@@ -5,7 +5,7 @@ export const ChatPaper = styled(Paper)({
     padding: 2,
     overflowY: "auto",
     borderRadius: "8px",
-    border: " 1px solid rgba(255, 255, 255, 0.60)",
+    border: " 1px solid #383c47",
     background: "rgba(203, 227, 255, 0.10)",
     boxShadow: "8px 16px 24px 0px rgba(0, 0, 0, 0.16)",
     backdropFilter: "blur(8px)",
