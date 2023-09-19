ac8e15e74e4a697f66e30aab742b39104328c13b
KomarovVladimir <komarov2142@gmail.com>
2023-09-19 04:37:02 +0300

    Update the chat styles

--- src/features/chat/Chat/index.tsx
+++ src/features/chat/Chat/index.tsx
@@ -1,71 +1,45 @@
-import {
-    Avatar,
-    Box,
-    IconButton,
-    List,
-    ListItem,
-    ListItemText,
-    TextField,
-    Typography,
-    styled,
-} from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
+import { Avatar, Box, ListItemText, Typography } from "@mui/material";
 
 import { useChat } from "./useChat";
-import { ChatPaper } from "./styled";
+import { ChatBox, ChatPaper, Message, MessageHeader } from "./styled";
 import { ChatInput } from "./ChatInput";
-
-const Message = styled(ListItem)({
-    alignItems: "start",
-    justifyContent: "start",
-});
-
-const MessageHeader = styled("div")({
-    display: "flex",
-    flexWrap: "nowrap",
-    alignItems: "center",
-    gap: "4px",
-});
-
-const ChatBox = styled(List)({
-    overflowY: "auto",
-});
 
 export const Chat = () => {
     const { messages } = useChat();
 
     return (
-        <>
-            <ChatPaper>
-                <ChatBox dense disablePadding>
-                    {messages &&
-                        messages?.map(
-                            ({ id, userName, date, text, avatar }) => (
-                                <Message key={id} dense disablePadding>
-                                    <Avatar
-                                        alt={userName}
-                                        src={avatar}
-                                        sx={{
-                                            marginRight: "1rem",
-                                            width: 38,
-                                            height: 38,
-                                        }}
-                                    />
-                                    <Box>
-                                        <MessageHeader>
-                                            <Typography fontWeight="600">
-                                                {userName}
-                                            </Typography>
-                                            <Typography>{date}</Typography>
-                                        </MessageHeader>
-                                        <ListItemText primary={text} />
-                                    </Box>
-                                </Message>
-                            )
-                        )}
-                </ChatBox>
-                <ChatInput />
-            </ChatPaper>
-        </>
+        <ChatPaper>
+            <ChatBox dense disablePadding>
+                {messages &&
+                    messages?.map(({ id, userName, date, text, avatar }) => (
+                        <Message key={id} dense disablePadding>
+                            <Avatar
+                                alt={userName}
+                                src={avatar}
+                                sx={{
+                                    marginRight: "1rem",
+                                    width: 38,
+                                    height: 38,
+                                }}
+                            />
+                            <Box>
+                                <MessageHeader>
+                                    <Typography
+                                        color="primary"
+                                        fontWeight="600"
+                                    >
+                                        {userName}
+                                    </Typography>
+                                    <Typography color="primary">
+                                        {date}
+                                    </Typography>
+                                </MessageHeader>
+                                <ListItemText primary={text} />
+                            </Box>
+                        </Message>
+                    ))}
+            </ChatBox>
+            <ChatInput />
+        </ChatPaper>
     );
 };
--- src/features/chat/Chat/styled.tsx
+++ src/features/chat/Chat/styled.tsx
@@ -1,15 +1,45 @@
-import { Paper, styled } from "@mui/material";
+import { List, ListItem, Paper, styled } from "@mui/material";
 
 export const ChatPaper = styled(Paper)({
     display: "flex",
     flexDirection: "column",
+    gap: ".5rem",
     justifyContent: "space-between",
     height: "inherit",
-    padding: "1.5rem 1rem",
+    padding: ".75rem 1rem",
     overflowY: "auto",
     borderRadius: "8px",
     border: " 1px solid #383c47",
     background: "rgba(203, 227, 255, 0.10)",
     boxShadow: "8px 16px 24px 0px rgba(0, 0, 0, 0.16)",
     backdropFilter: "blur(8px)",
+});
+
+export const Message = styled(ListItem)({
+    alignItems: "start",
+    justifyContent: "start",
+});
+
+export const MessageHeader = styled("div")({
+    display: "flex",
+    flexWrap: "nowrap",
+    alignItems: "center",
+    gap: "4px",
+});
+
+export const ChatBox = styled(List)({
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    overflowY: "auto",
+    paddingLeft: ".5rem",
+    paddingRight: "0.4rem",
+
+    "&::-webkit-scrollbar": {
+        width: "0.4em",
+    },
+    "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "rgba(0, 0, 0, 0.20)",
+        borderRadius: "0.2em",
+    },
 });
