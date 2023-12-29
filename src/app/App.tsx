dff0cd1c375f57fd940e2f0d9eefef581b14b42e
KomarovVladimir <komarov2142@gmail.com>
2023-12-29 18:56:54 +0300

    Add a socket io instance

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,16 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { io } from "socket.io-client";
 
 // import "services/server";
 import { AuthProvider } from "features";
 import { AppRoutes } from "routing";
 
 import { store } from "./store";
+
+//TODO: Move sockets io
+const socket = io("http://localhost:5000");
 
 const darkTheme = createTheme({
     palette: {
