2251d3bf1f77410ab8059e4bb173ebb3762afd58
KomarovVladimir <komarov2142@gmail.com>
2024-02-13 19:50:06 +0300

    Update webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,55 +3,59 @@ const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-    entry: path.resolve(__dirname, "../src/index.tsx"),
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        alias: {
-            app: path.resolve(__dirname, "../src/app"),
-            assets: path.resolve(__dirname, "../src/assets"),
-            common: path.resolve(__dirname, "../src/common"),
-            components: path.resolve(__dirname, "../src/components"),
-            features: path.resolve(__dirname, "../src/features"),
-            routing: path.resolve(__dirname, "../src/routing"),
+module.exports = (env, argv) => {
+    return {
+        entry: path.resolve(__dirname, "../src/index.tsx"),
+        mode: env.production ? "production" : "development",
+        devtool: env.production ? "source-map" : "eval",
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "../dist"),
         },
-    },
-    plugins: [
-        new ESLintPlugin({
-            extensions: ["js", "jsx", "ts", "tsx"],
-            overrideConfig: {
-                extends: [
-                    "eslint:recommended",
-                    "plugin:@typescript-eslint/recommended",
-                ],
-                parser: "@typescript-eslint/parser",
-                plugins: ["@typescript-eslint"],
-                root: true,
-                rules: {
-                    "@typescript-eslint/no-unused-vars": "warn",
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
                 },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"],
+            alias: {
+                app: path.resolve(__dirname, "../src/app"),
+                assets: path.resolve(__dirname, "../src/assets"),
+                common: path.resolve(__dirname, "../src/common"),
+                components: path.resolve(__dirname, "../src/components"),
+                features: path.resolve(__dirname, "../src/features"),
+                routing: path.resolve(__dirname, "../src/routing"),
             },
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "../public/index.html"),
-        }),
-        new CleanWebpackPlugin(),
-    ],
+        },
+        plugins: [
+            new ESLintPlugin({
+                extensions: ["js", "jsx", "ts", "tsx"],
+                overrideConfig: {
+                    extends: [
+                        "eslint:recommended",
+                        "plugin:@typescript-eslint/recommended",
+                    ],
+                    parser: "@typescript-eslint/parser",
+                    plugins: ["@typescript-eslint"],
+                    root: true,
+                    rules: {
+                        "@typescript-eslint/no-unused-vars": "warn",
+                    },
+                },
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "../public/index.html"),
+            }),
+            new CleanWebpackPlugin(),
+        ],
+    };
 };
