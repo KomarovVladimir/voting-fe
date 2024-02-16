eae277b8082cf71799e1dfd100408699c233e4fb
KomarovVladimir <komarov2142@gmail.com>
2024-02-16 16:35:16 +0300

    Fix public data copying

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path")
 const ESLintPlugin = require("eslint-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = (env, argv) => {
     return {
@@ -62,6 +63,17 @@ module.exports = (env, argv) => {
             }),
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, "../public/index.html"),
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, "../public"),
+                        to: path.resolve(__dirname, "../dist"),
+                        globOptions: {
+                            ignore: ["**/index.html"],
+                        },
+                    },
+                ],
             }),
             new CleanWebpackPlugin(),
         ],
