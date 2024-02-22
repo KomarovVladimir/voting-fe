71cb4811d92ee1adce66e8b7371134be828da514
KomarovVladimir <komarov2142@gmail.com>
2024-02-22 15:53:06 +0300

    Update the rooms layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export * from "./layouts";
 export * from "./AppBar";
 export * from "./Paper";
 export * from "./Title";
-export * from "./Avatar";
 export * from "./ListItem";
--- src/features/roomsManager/components/RoomsManager.tsx
+++ src/features/roomsManager/components/RoomsManager.tsx
@@ -1,38 +1,25 @@
-import { Grid, List } from "@mui/material";
+import { Avatar, Grid, List } from "@mui/material";
 
-import { Avatar, ListItem } from "components";
+import { ListItem } from "components";
 
 import { CreationDialog } from "./CreationDialog";
-import { JoiningDialog } from "./JoiningDialog";
 
 import { useRoomsManager } from "../hooks";
 
 export const RoomsManager = () => {
-    const {
-        rooms,
-        creationOpen,
-        joiningOpen,
-        handleOpen,
-        handleClose,
-        handleJoiningOpen,
-        handleJoiningClose
-    } = useRoomsManager();
+    const { rooms, creationOpen, handleOpen, handleClose } = useRoomsManager();
 
     return (
         <List>
-            <JoiningDialog onClose={handleJoiningClose} open={joiningOpen} />
             <CreationDialog onClose={handleClose} open={creationOpen} />
             <Grid container spacing={2}>
                 {rooms?.map((props, index) => (
                     <ListItem
                         key={index}
-                        icon={<Avatar avatarText={index.toString()} />}
+                        icon={<Avatar>{index.toString()}</Avatar>}
                     />
                 ))}
-                <ListItem
-                    icon={<Avatar avatarText="+" />}
-                    onClick={handleOpen}
-                />
+                <ListItem icon={<Avatar>+</Avatar>} onClick={handleOpen} />
             </Grid>
         </List>
     );
